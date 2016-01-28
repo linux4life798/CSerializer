f75d44e0b1096502f9a5b69fcf28a7389bcb9852
Craig Hesling <craig@hesling.com>
2016-01-28 01:41:17 -0500

    Housekeeping

--- serializer.h
+++ serializer.h
@@ -90,11 +90,11 @@ data_type_t serial_iit_get_type(serial_item_iterator_t *it);
  *-----------------------------------*/
 
 /* Get the total size of serialized data - for copying the serialized data */
-size_t serial_data_size(serial_data_t sdata);
+size_t serial_data_size    (serial_data_t sdata);
 
-size_t serial_item_count(serial_data_t sdata);
-int serial_item_get_int(serial_data_t sdata, size_t index);
-void serial_print_table(serial_data_t sdata);
-void serial_print_items(serial_data_t sdata);
+size_t serial_item_count   (serial_data_t sdata);
+int    serial_item_get_int (serial_data_t sdata, size_t index);
+void   serial_print_table  (serial_data_t sdata);
+void   serial_print_items  (serial_data_t sdata);
 
 #endif /* SERIALIZER_SERIALIZER_H_ */
